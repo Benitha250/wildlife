502d397a5bb170fc42eb5e469f7d7c69862e8f83
Benitha250 <benithauwase05@gmail.com>
2020-11-30 11:14:43 +0200

    add DB

---
+++ src/main/java/DB.java
@@ -0,0 +1,5 @@
+import org.sql2o.Sql2o;
+
+public class DB {
+    public static Sql2o sql2o = new Sql2o( "jdbc:postgresql://ec2-34-200-72-77.compute-1.amazonaws.com:5432/dbdm5s1n8hejg", "lwkrvliubcpfda", "830bc954504472192aa7d9be1b7c1b4a1e5e06e0a1acb70d3507660c180bf5da");
+}
